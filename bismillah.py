ab7941278baa5805cea93a72cd868b2ad5befd9b
fivegroup05 <132416413+fivegroup05@users.noreply.github.com>
2023-05-19 13:39:46 +0700

    Add files via upload

--- bismillah.py
+++ bismillah.py
@@ -18,11 +18,13 @@ def main():
     elif choice == "kalkulator":
         st.subheader("Kalkulator")
 
-def input pengertian():
-    st.subheader("Pengertian")
-    st.write("Chemical Oxygen Demand (COD) adalah ukuran kuantitatif dari jumlah oksigen yang diperlukan untuk mengoksidasi bahan organik dalam suatu sampel air atau limbah. Pengukuran kadar COD umumnya dilakukan dalam bidang pengolahan air limbah dan pengujian kualitas air.")
+def input_pengertian_cod():
+    pengertian_cod = input("Chemical Oxygen Demand (COD) adalah ukuran kuantitatif dari jumlah oksigen yang diperlukan untuk mengoksidasi bahan organik dalam suatu sampel air atau limbah. Pengukuran kadar COD umumnya dilakukan dalam bidang pengolahan air limbah dan pengujian kualitas air.") 
+    return pengertian_cod
 
+# Memanggil fungsi input_pengertian_cod dan menyimpan hasilnya
+teks_pengertian_cod = input_pengertian_cod()
 
+# Menampilkan teks pengertian COD yang dimasukkan
+print("Pengertian Chemical Oxygen Demand (COD):", teks_pengertian_cod)
 
-if __name__ == "__main__":
-    main()
