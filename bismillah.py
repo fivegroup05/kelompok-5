47631436c8b4f93f2395da742ee1ffe12c0f64d7
fivegroup05 <132416413+fivegroup05@users.noreply.github.com>
2023-05-19 14:26:39 +0700

    Add files via upload

--- bismillah.py
+++ bismillah.py
@@ -22,9 +22,7 @@ def input_pengertian_cod():
     pengertian_cod = input("Chemical Oxygen Demand (COD) adalah ukuran kuantitatif dari jumlah oksigen yang diperlukan untuk mengoksidasi bahan organik dalam suatu sampel air atau limbah. Pengukuran kadar COD umumnya dilakukan dalam bidang pengolahan air limbah dan pengujian kualitas air.") 
     return pengertian_cod
 
-# Memanggil fungsi input_pengertian_cod dan menyimpan hasilnya
 teks_pengertian_cod = input_pengertian_cod()
 
-# Menampilkan teks pengertian COD yang dimasukkan
 print("Pengertian Chemical Oxygen Demand (COD):", teks_pengertian_cod)
 
